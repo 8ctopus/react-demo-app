83e1c6134b3053aaad852037cf2ecc31106213eb
8ctopus <hello@octopuslabs.io>
2022-04-08 20:43:24 +0500

    added: expense item uses props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Let's get started!</h1>
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem date={ new Date(2022,3,4) } description="car insurance" amount="200" />
+      <ExpenseItem date={ new Date(2022,4,4) } description="telephone" amount="100" />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,9 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return <div className="expense-item">
-    <div className="expense-item__date">Date</div>
-    <div className="expense-item__description">Description</div>
-    <div className="expense-item__amount">Amount</div>
+    <div className="expense-item__date">{props.date.toISOString()}</div>
+    <div className="expense-item__description">{props.description}</div>
+    <div className="expense-item__amount">{props.amount}</div>
   </div>;
 }
