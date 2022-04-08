862d51b55f368a7dfab6efc2b3b7333b987823c5
8ctopus <hello@octopuslabs.io>
2022-04-08 21:02:37 +0500

    added: expense date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,9 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItem(props) {
   return <div className="expense-item">
-    <div className="expense-item__date">{props.date.toISOString()}</div>
+    <ExpenseDate date={ props.date } />
     <div className="expense-item__description">{props.description}</div>
     <div className="expense-item__amount">{props.amount}</div>
   </div>;
