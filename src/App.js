198ef6954cbf29bdde3314442452450b096b5700
8ctopus <hello@octopuslabs.io>
2022-04-08 20:14:35 +0500

    added: first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import H2 from './components/H2.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's get started!</h1>
+      <H2 />
     </div>
   );
 }
---
+++ src/components/H2.js
@@ -0,0 +1,7 @@
+import "./H2.css";
+
+export default function H2() {
+  return <div>
+      <h2>Red</h2>
+    </div>;
+}
