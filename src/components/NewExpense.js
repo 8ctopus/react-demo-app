b3a74247ea3b451a413049e26fc32cf35f47155b
8ctopus <hello@octopuslabs.io>
2022-04-11 14:43:59 +0500

    added: new expense event handlers

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,14 +1,38 @@
+import {useState} from "react";
+
 export default function NewExpense() {
+    const [description, setDescription] = useState('test');
+    const [amount, setAmount] = useState('10');
+    const [date, setDate] = useState('2022-04-10');
+
+    const onChangeDescriptionHandler = (event) => {
+        setDescription(event.target.value);
+    };
+
+    const onChangeAmountHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
+    const onChangeDateHandler = (event) => {
+        setDate(event.target.value);
+    };
+
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        console.log("form clicked!", event);
+        console.log("form clicked!", {
+            description,
+            amount,
+            date,
+        });
+
+        setDescription("submited");
     };
 
     return <div>
         <form onSubmit={onSubmitHandler} >
-            <input type="text" name="description" defaultValue="test" />
-            <input type="number" name="amount" defaultValue="10" />
-            <input type="date" name="date" defaultValue="2022-04-10" />
+            <input type="text" name="description" onChange={onChangeDescriptionHandler} value={description} />
+            <input type="number" name="amount" onChange={onChangeAmountHandler} value={amount} />
+            <input type="date" name="date" onChange={onChangeDateHandler} value={date} />
             <button type="submit" />
         </form>
     </div>;
