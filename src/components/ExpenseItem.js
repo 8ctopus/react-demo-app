2ed33d1da3859b368fe1e9e0c4cc2d299cfc1405
8ctopus <hello@octopuslabs.io>
2022-04-11 12:19:33 +0500

    added: use state to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
+import {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItem(props) {
+  const [description, setDescription] = useState(props.description);
+
   function onClickHandler() {
-    console.log("clicked");
+    setDescription("clicked");
   }
 
   return <div className="expense-item">
     <ExpenseDate date={ props.date } />
-    <div className="expense-item__description">{props.description}</div>
+    <div className="expense-item__description">{description}</div>
     <div className="expense-item__amount">{props.amount}</div>
     <button onClick={onClickHandler} />
   </div>;
